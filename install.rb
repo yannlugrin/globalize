9f860442e64da4d6a741bf1f60b0e02cb96029da
Saimon Moore <saimonmoore@gmail.com>
2007-03-05 08:35:51 +0000

    --Trying fork

--- install.rb
+++ install.rb
@@ -1,3 +1,7 @@
 APP_ROOT = File.join(File.dirname(__FILE__), '../../../')
-result = `rake -f #{APP_ROOT}Rakefile globalize:upgrade_schema_to_1_dot_2`
-puts result && exit!
+result = nil
+fork do
+  result = `rake -f #{APP_ROOT}Rakefile globalize:upgrade_schema_to_1_dot_2`
+end
+Process.wait
+puts result && exit
