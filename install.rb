7206d44ad999245ad895cdfe3a2853348b3c6a44
Saimon Moore <saimonmoore@gmail.com>
2007-03-05 08:16:23 +0000

    --testing install.rb

--- install.rb
+++ install.rb
@@ -1,3 +1,2 @@
 APP_ROOT = File.join(File.dirname(__FILE__), '../../../')
-puts APP_ROOT
-`cd #{APP_ROOT} && rake globalize:upgrade_schema_to_1_dot_2`
+`rake -f #{APP_ROOT}Rakefile globalize:upgrade_schema_to_1_dot_2`
