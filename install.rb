03da6155b98514165982ba4935a1374f3471868d
Saimon Moore <saimonmoore@gmail.com>
2007-03-05 08:38:48 +0000

    --testing install.rb

--- install.rb
+++ install.rb
@@ -1,7 +1,3 @@
 APP_ROOT = File.join(File.dirname(__FILE__), '../../../')
-result = nil
-fork do
-  result = `rake -f #{APP_ROOT}Rakefile globalize:upgrade_schema_to_1_dot_2`
-end
-Process.wait
+result = `rake -f #{APP_ROOT}Rakefile globalize:upgrade_schema_to_1_dot_2`
 puts result && exit
