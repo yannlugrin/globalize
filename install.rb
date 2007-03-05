677eef80b6ab6d89a5b80a1ddee0e728047d871e
Saimon Moore <saimonmoore@gmail.com>
2007-03-05 08:26:04 +0000

    --More install hook testing

--- install.rb
+++ install.rb
@@ -1,2 +1,3 @@
 APP_ROOT = File.join(File.dirname(__FILE__), '../../../')
-puts `rake -f #{APP_ROOT}Rakefile globalize:upgrade_schema_to_1_dot_2`
+result = `rake -f #{APP_ROOT}Rakefile globalize:upgrade_schema_to_1_dot_2`
+puts result
