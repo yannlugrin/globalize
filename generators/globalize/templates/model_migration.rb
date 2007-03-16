368727f9c05e72212ce7ecf37242d306862a8ffa
Lourens Naudé <lourens@methodmissing.com>
2007-03-16 11:42:19 +0000

    migration column defaults to empty string if AR column default is blank

--- generators/globalize/templates/model_migration.rb
+++ generators/globalize/templates/model_migration.rb
@@ -4,7 +4,7 @@ class <%= migration_class_name %> < ActiveRecord::Migration
 <% attributes_for_migrations.each_key do |key| -%>
     #Fields for <%= key.split('/').first %>
 <% attributes_for_migrations[key].each do |field_data| -%>
-    add_column :<%= key.split('/').last %>, :<%= field_data[0] %>, :<%= field_data[1] %><%= field_data[2].nil? ? "" : ", :default => #{field_data[2]}" %>
+    add_column :<%= key.split('/').last %>, :<%= field_data[0] %>, :<%= field_data[1] %><%= ", :default => #{field_data[2].blank? ? "''" : field_data[2]}" %>
 <% end -%>
 <% end -%>
   end
