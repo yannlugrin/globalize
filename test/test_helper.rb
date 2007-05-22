594412e2730f8dbf50cf408b3a0dd566fe05740c
Joshua Sierles <jsierles@gmail.com>
2007-05-22 22:25:53 +0000

    backport default test configuration from trunk

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,13 +2,15 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + '/../../../../config/environment')
 require 'logger'
 require 'test_help'
+require 'stringio'
 
-plugin_path = RAILS_ROOT + "/vendor/plugins/globalize"
-
-config_location = plugin_path + "/test/config/database.yml"
+plugin_path = File.expand_path(File.dirname(__FILE__)+"/../")
+config_location = RAILS_ROOT + "/config/database.yml"
 
 config = YAML::load(ERB.new(IO.read(config_location)).result)
-ActiveRecord::Base.logger = Logger.new(plugin_path + "/test/log/test.log")
+log_file = plugin_path + "/test/log/test.log"
+FileUtils.touch(log_file) unless File.exist?(log_file)
+ActiveRecord::Base.logger = Logger.new(log_file)
 ActiveRecord::Base.establish_connection(config['test'])
 
 schema_file = plugin_path + "/test/db/schema.rb"
